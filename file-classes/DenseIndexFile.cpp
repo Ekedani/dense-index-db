2353baa6c65659a911175a08bd829597f50ae3b3
Vladlen <ilusenkovladlen@gmail.com>
2021-10-20 17:41:38 +0300

    DenseIndexFile refactoring

--- file-classes/DenseIndexFile.cpp
+++ file-classes/DenseIndexFile.cpp
@@ -1,1 +1,48 @@
 #include "DenseIndexFile.h"
+
+bool DenseIndexFile::remove(unsigned int keyValue) {
+    auto indexSearchResult = indexSegment->get(keyValue);
+    if (indexSearchResult == nullptr) {
+        return false;
+    }
+    dataSegment->remove(indexSearchResult->dataPointer);
+    return indexSegment->remove(keyValue);
+}
+
+string DenseIndexFile::get(unsigned int keyValue) {
+    auto indexSearchResult = indexSegment->get(keyValue);
+    if (indexSearchResult == nullptr) {
+        return "";
+    }
+    auto dataSearchResult = dataSegment->get(indexSearchResult->dataPointer);
+    return dataSearchResult->dataValue;
+}
+
+bool DenseIndexFile::add(unsigned int keyValue, string dataValue) {
+    if (indexSegment->get(keyValue) != nullptr) {
+        return false;
+    }
+    auto dataPointer = dataSegment->add(keyValue, std::move(dataValue));
+    indexSegment->add(keyValue, dataPointer);
+    return true;
+}
+
+bool DenseIndexFile::edit(unsigned int keyValue, string dataValue) {
+    auto indexSearchResult = indexSegment->get(keyValue);
+    if (indexSearchResult == nullptr) {
+        return false;
+    }
+    dataSegment->edit(indexSearchResult->dataPointer, std::move(dataValue));
+}
+
+DenseIndexFile::DenseIndexFile() {
+    indexSegment = new IndexSegment();
+    dataSegment = new DataSegment();
+}
+
+void DenseIndexFile::output() {
+    cout << "Index segment: " << '\n';
+    indexSegment->output();
+    cout << "Data segment: " << '\n';
+    dataSegment->output();
+}
--- file-classes/DenseIndexFile.h
+++ file-classes/DenseIndexFile.h
@@ -1,58 +1,20 @@
-#include <utility>
-
 #include "DataSegment.h"
 #include "IndexSegment.h"
 
+//Program representation of dense-index file
 class DenseIndexFile {
 private:
     IndexSegment* indexSegment;
     DataSegment* dataSegment;
 public:
-    DenseIndexFile(){
-        indexSegment = new IndexSegment();
-        dataSegment = new DataSegment();
-    }
+    DenseIndexFile();
 
-    bool add(unsigned int keyValue, string dataValue){
-        if(indexSegment->get(keyValue) != nullptr){
-            return false;
-        }
-        auto dataPointer = dataSegment->add(keyValue, std::move(dataValue));
-        indexSegment->add(keyValue, dataPointer);
-        return true;
-    }
+    //Database methods
+    bool add(unsigned int keyValue, string dataValue);
+    string get(unsigned int keyValue);
+    bool remove(unsigned int keyValue);
+    bool edit(unsigned int keyValue, string dataValue);
 
-    string get(unsigned int keyValue){
-        auto indexSearchResult = indexSegment->get(keyValue);
-        if(indexSearchResult == nullptr){
-            return "";
-        }
-        auto dataSearchResult = dataSegment->get(indexSearchResult->dataPointer);
-        return dataSearchResult->dataValue;
-    }
-
-    bool remove(unsigned int keyValue){
-        auto indexSearchResult = indexSegment->get(keyValue);
-        if(indexSearchResult == nullptr){
-            return false;
-        }
-        dataSegment->remove(indexSearchResult->dataPointer);
-        return indexSegment->remove(keyValue);
-    }
-
-    bool edit(unsigned int keyValue, string dataValue){
-        auto indexSearchResult = indexSegment->get(keyValue);
-        if(indexSearchResult == nullptr){
-            return false;
-        }
-        dataSegment->edit(indexSearchResult->dataPointer, std::move(dataValue));
-    }
-
-    void output(){
-        cout << "Index segment: " << '\n';
-        indexSegment->output();
-        cout << "Data segment: " << '\n';
-        dataSegment->output();
-    }
+    void output();
 
 };
