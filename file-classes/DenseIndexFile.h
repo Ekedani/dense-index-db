513b716a2687efca4fef7e737a402b1cf1291f0b
Vladlen <ilusenkovladlen@gmail.com>
2021-10-20 13:10:06 +0300

    Implemented add, get, remove and edit methods for DenseIndexFile

--- file-classes/DenseIndexFile.h
+++ file-classes/DenseIndexFile.h
@@ -1,11 +1,51 @@
+#include <utility>
+
 #include "DataSegment.h"
 #include "IndexSegment.h"
 
 class DenseIndexFile {
 private:
-    IndexSegment indexes;
-    DataSegment dataSegment;
+    IndexSegment* indexSegment;
+    DataSegment* dataSegment;
 public:
-    //TODO: add, get, edit, delete
+    DenseIndexFile(){
+        indexSegment = new IndexSegment();
+        dataSegment = new DataSegment();
+    }
+
+    bool add(unsigned int keyValue, string dataValue){
+        if(indexSegment->get(keyValue) != nullptr){
+            return false;
+        }
+        auto dataPointer = dataSegment->add(keyValue, std::move(dataValue));
+        indexSegment->add(keyValue, dataPointer);
+        return true;
+    }
+
+    string get(unsigned int keyValue){
+        auto indexSearchResult = indexSegment->get(keyValue);
+        if(indexSearchResult == nullptr){
+            return "";
+        }
+        auto dataSearchResult = dataSegment->get(indexSearchResult->dataPointer);
+        return dataSearchResult->dataValue;
+    }
+
+    bool remove(unsigned int keyValue){
+        auto indexSearchResult = indexSegment->get(keyValue);
+        if(indexSearchResult == nullptr){
+            return false;
+        }
+        dataSegment->remove(indexSearchResult->dataPointer);
+        return indexSegment->remove(keyValue);
+    }
+
+    bool edit(unsigned int keyValue, string dataValue){
+        auto indexSearchResult = indexSegment->get(keyValue);
+        if(indexSearchResult == nullptr){
+            return false;
+        }
+        dataSegment->edit(indexSearchResult->dataPointer, std::move(dataValue));
+    }
 
 };
