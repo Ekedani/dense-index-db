d1bf27b1302c90e20fffb8216481ac8eaafa3b6e
Vladlen <ilusenkovladlen@gmail.com>
2021-10-20 21:23:53 +0300

    Transported code from .cpp to .h file

--- helpers/RandomDataGenerator.cpp
+++ helpers/RandomDataGenerator.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "RandomDataGenerator.h"
+
+unsigned int RandomDataGenerator::generateRandomKey(int num) {
+    std::default_random_engine generator(time(nullptr) + num);
+    std::uniform_int_distribution<int> distribution(0, INT_MAX);
+    return distribution(generator);
+}
+
+std::string RandomDataGenerator::generateRandomData(int num) {
+    srand(time(nullptr) + num);
+    char symbol;
+    std::string result;
+    for (int i = 0; i < 100; ++i) {
+        symbol = (rand() % 86) + 41;
+        //Just to make sure that .csv file will be okay
+        if (symbol == ',') {
+            symbol = '0';
+        }
+        result += symbol;
+    }
+    return result;
+}
