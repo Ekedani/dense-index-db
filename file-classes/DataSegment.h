1caaf43306d98a3c44b8d0b47fc3680d637452f0
Vladlen <ilusenkovladlen@gmail.com>
2021-10-20 15:27:30 +0300

    Implemented reading file (DataSegment)

--- file-classes/DataSegment.h
+++ file-classes/DataSegment.h
@@ -10,6 +10,14 @@ struct DataRecord {
     unsigned int keyValue;
     string dataValue;
     bool notDeleted;
+
+    void parseString(string dataLine){
+        this->keyValue = stoi(dataLine.substr(0, dataLine.find(',')));
+        dataLine.erase(0, dataLine.find(',') + 1);
+        this->dataValue = dataLine.substr(1, dataLine.find("\","));
+        dataLine.erase(0, dataLine.find("\",") + 2);
+        this->notDeleted = stoi(dataLine);
+    }
 };
 
 class DataSegment {
@@ -17,6 +25,19 @@ private:
     vector<DataRecord *> fileData;
 public:
     DataSegment() {
+    }
+
+    void readFile(){
+        ifstream filePtr;
+        filePtr.open("D:\\Programming\\dense-index-db\\data\\data_seg.csv");
+        while(!filePtr.eof()){
+            string curDataLine;
+            getline(filePtr, curDataLine);
+            auto newDataRecord = new DataRecord;
+            newDataRecord->parseString(curDataLine);
+            fileData.push_back(newDataRecord);
+        }
+        filePtr.close();
     }
 
     void saveFile() {
